2d2304b865b4189e7bb0840642b1c91721e46156
VIVIVIciiEND <akirmandinara@gmail.com>
2026-03-04 18:05:43 +0300

    Step 10: browser console basics and simple math

--- main.js
+++ main.js
@@ -1,6 +1,149 @@
-let age = 20 ; 
-let  name = "Dinara";
-let isStudent = true ;
-console.log("Name: " , name);
-console.log("Age: " , age );
-console.log("Is student " , isStudent) ; 
+// // let age = 20 ; 
+// // let  name = "Dinara";
+// // let isStudent = true ;
+// // console.log("Name: " , name);
+// // console.log("Age: " , age );
+// // console.log("Is student " , isStudent) ; 
+// // let value = 10 ; 
+// // console.log(value); 
+// // value = "теперь это строока" ; 
+// // console.log(value);
+// // value = true ; 
+// // console.log(value);
+// let userName = "Aлексей";
+// console.log('привет ${userName}');
+// let price = 99.99;
+// let temperature = -15 ; 
+// let infinity = 1 / 0 ;
+// let notANumber =0/0;
+// console.log(0.1 + 0.2);
+// let bigNumber = 9007543958309458n ;
+// let huge = BigInt("4900490394294453453535");
+// let isAlive = true ; 
+// let isWorking = false ; 
+// let isAdult = age >= 18;
+// let x ;
+// let y = undefined;
+// let userData = null ; 
+// let id = Symbol("id");
+// let person = {
+//     name : "станислва" ,
+//     age : 30 , 
+//     isStudent : false, 
+//     sayHello: function(){
+//         console.log("Привет");
+//     }, 
+// };
+// console.log(person.name)
+// let fruits = ["яблоко" , "банан" , "апельсин"];
+// let numbers = [1,2,3,4,5];
+// let mixed = ["текст" , 42 , true , null]; 
+
+// function sum(a , b){
+//     return a + b ; 
+// }
+
+// let multiply = function ( x ,y){
+//     return x * y ; 
+// }
+// console.log(sum(5 ,3))
+
+// let now = new Date();
+// let birtday = new Date("2007-07-28")
+
+// let a = 10 ; 
+// let b = 3 ;
+// console.log(a+b) ; 
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// console.log(10 + "5");
+// console.log("10" - 5 ); 
+
+// const numbersArray = [1,2,3];
+// numbersArray[0] = 10 ;
+// console.log(numbersArray);
+// numbersArray = [5,6,7] ; 
+
+// const persons = {name : "Dinara" , age : 18};
+// persons.age = 18 ; 
+// persons.city = "Volgograd";
+// console.log(persons)
+// person = {name: "Stas"};
+
+// console.log(typeof "текст");
+// console.log(typeof 42);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof function () {});
+
+// let numberx = null ; 
+// console.log(numberx === null) ; 
+
+// //практическое задание 1
+// let newPrice = 4 ;
+// console.log(newPrice);
+// console.log(typeof newPrice);
+// newPrice = "rrr" ;
+// console.log(newPrice);
+// console.log(typeof newPrice);
+
+// let num = 42 ;
+// let str = String(num) ;
+// let str2 = num.toString();
+// let str3 = "" +num ;
+// let strNum = "123" ; 
+// let int = Number(strNum); 
+// let int2 = parseInt("42.5");
+// let float = parseFloat("3.14");
+// let int3 = + "99"
+
+// let bool1 = Boolean(1) ; 
+// let bool2 = !!1 ; 
+// let bool3 = Boolean(0) ; 
+// let bool4 = Boolean("");
+
+// console.log("5" +3);
+// console.log("5" -3);
+// console.log("5" * "2");
+// console.log(true +1);
+// console.log(true +1);
+// console.log(null +1);
+// console.log(undefined+1);
+
+// console.log(5=="5");
+// console.log(5==="5");
+// console.log(0==false);
+// console.log(0===false);
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// let obj1 = {name : "john"} ;
+// let obj2 = {neme : "john"} ;
+// console.log(obj1 == obj2) ;
+// console.log(obj1 === obj2) ;
+
+// let obj3 = obj2;
+// console.log(obj1 ===obj3);
+
+// let arr1 = [1,2,3];
+// let arr2 = [1,2,3];
+// console.log(arr1 === arr2) ; 
+
+//практическое задание 2 ; 
+let x1 = 10 ;
+let y1 = 2 ;
+console.log(x1 + y1);
+console.log(x1 - y1);
+console.log(x1 * y1);
+console.log(x1  / y1);
+
+x1 = "10" ; 
+console.log(x1 + y1);
+console.log(x1 - y1);
+console.log(x1 * y1);
+console.log(x1  / y1);
